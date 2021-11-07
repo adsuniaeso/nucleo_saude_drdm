d5bd500855b56928b6eab63686546b4f02d4344f
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2021-11-07 15:19:42 -0300

    Criação do Projeto.

---
+++ cmake-build-debug/CMakeFiles/nucleo_saude_drdm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nucleo_saude_drdm.dir/main.c.obj"
+  "libnucleo_saude_drdm.dll.a"
+  "nucleo_saude_drdm.exe"
+  "nucleo_saude_drdm.exe.manifest"
+  "nucleo_saude_drdm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nucleo_saude_drdm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/nucleo_saude_drdm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nucleo_saude_drdm.
+# This may be replaced when dependencies are built.
