9de95a5db242b8e2322dbfefc15a038e9acfce02
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2021-11-08 17:30:29 -0300

    Atualização do cadastro de clientes.

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <windows.h>
+#include <locale.h>
 
 typedef struct {
-    char dia[2], mes[2], ano[4];
+    int dia, mes, ano;
 } Data;
 
 typedef struct {
@@ -13,7 +14,7 @@ typedef struct {
 } Dependente;
 
 typedef struct {
-    char cpf[11], nome[30], email[30], telefone[11];
+    char cpf[11], nome[25], email[35], telefone[11];
     int sexo, plano;
     float valorPlano;
     Data data_nascimento, data_vencimento;
@@ -36,7 +37,10 @@ void inserir();
 
 void listagem();
 
+//float calculaValorPlano(Titular titular);
+
 main() {
+    setlocale(LC_ALL, "Portuguese");
     int opcao;
     abre_arquivo();
     do {
@@ -48,7 +52,7 @@ main() {
         printf("3 - Remover Cliente\n");
         printf("4 - Listagem Geral\n");
         printf("5 - Listagem por Plano\n");
-        printf("6 - Listagem dos Vencimentos do Plano de Saúde do Mees\n");
+        printf("6 - Listagem dos Vencimentos do Plano de Saude do Mes\n");
         printf("0 - Sair\n");
         linha();
         printf("Informe a opcao desejada: ");
@@ -76,6 +80,8 @@ main() {
                 break;
             case 0:
                 fclose(base_clientes);
+                printf("\nAte a Proxima !\n\n");
+                system("pause");
                 break;
             default:
                 printf("\n\n\aOpcao invalida!");
@@ -98,36 +104,43 @@ void cabecalho() {
 }
 
 void abre_arquivo() {
-    base_clientes = fopen("..\\base_clientes.bin", "a+b");
+    base_clientes = fopen("..\\base_clientes.txt", "a");
 }
 
 Dependente popularDependente() {
     Dependente dependente;
+    linha();
     printf("\nCadastrar novo dependente\n");
-    printf("\nCPF: ");
-    scanf("%s", &dependente.cpf);
+    linha();
+//    printf("\nCPF: ");
+//    scanf("%s", &dependente.cpf);
     printf("\nNome: ");
     fflush(stdin);
     gets(dependente.nome);
-    printf("\nData de Nascimento:\nDia(xx):");
-    scanf("%s", &dependente.data_nascimento.dia);
-    printf("\nMes(xx):");
-    scanf("%s", &dependente.data_nascimento.mes);
-    printf("\nAno(xxxx):");
-    scanf("%s", &dependente.data_nascimento.ano);
+    printf("\nData de Nascimento:\nDia:");
+    scanf("%d", &dependente.data_nascimento.dia);
+    printf("\nMes:");
+    scanf("%d", &dependente.data_nascimento.mes);
+    printf("\nAno:");
+    scanf("%d", &dependente.data_nascimento.ano);
     return dependente;
 }
 
 void inserir() {
     int resposta;
     int temDependente;
+
+    SYSTEMTIME time;
+    GetSystemTime(&time);
+
     do {
+        system("cls");
         printf("\n");
         linha();
         printf("                                               Cadastrar novo Cliente\n");
         linha();
-        printf("\n\nCPF: ");
-        scanf("%s", &titular_aux.cpf);
+//        printf("\n\nCPF: ");
+//        scanf("%s", &titular_aux.cpf);
         printf("\nNome: ");
         fflush(stdin);
         gets(titular_aux.nome);
@@ -138,12 +151,12 @@ void inserir() {
         printf("\nE-mail: ");
         fflush(stdin);
         gets(titular_aux.email);
-        printf("\nData de Nascimento:\nDia(xx):");
-        scanf("%s", &titular_aux.data_nascimento.dia);
-        printf("\nMes(xx):");
-        scanf("%s", &titular_aux.data_nascimento.mes);
-        printf("\nAno(xxxx):");
-        scanf("%s", &titular_aux.data_nascimento.ano);
+        printf("\nData de Nascimento:\nDia:");
+        scanf("%d", &titular_aux.data_nascimento.dia);
+        printf("\nMes:");
+        scanf("%d", &titular_aux.data_nascimento.mes);
+        printf("\nAno:");
+        scanf("%d", &titular_aux.data_nascimento.ano);
         printf("\nPlano: \n1 - Ouro\n2 - Diamante\n3 - Prata\n4 - Esmeralda\nEscolha:");
         scanf("%d", &titular_aux.plano);
 
@@ -157,12 +170,18 @@ void inserir() {
             x++;
         } while (temDependente == 1);
 
+//        titular_aux.valorPlano = calculaValorPlano(titular_aux);
+
+        titular_aux.data_vencimento.dia = time.wDay;
+        titular_aux.data_vencimento.mes = time.wMonth;
+        titular_aux.data_vencimento.ano = (time.wYear + 1);
+
         fseek(base_clientes, 0, SEEK_END);
         fwrite(&titular_aux, sizeof(Titular), 1, base_clientes);
         printf("\nCliente cadastrado com sucesso!\n");
         printf("\nDeseja cadastrar outro Cliente?\n1 - Sim\n0 - Nao\nResposta:");
         scanf("%d", &resposta);
-        if(resposta == 1) {
+        if (resposta == 1) {
             memset(titular_aux.dependentes, 0, sizeof(Dependente));
         }
     } while (resposta == 1);
@@ -173,7 +192,7 @@ void listagem() {
     linha();
     printf("                                                  Listagem Geral\n");
     linha();
-    printf("CPF         Nome            Sexo Fone        Email                          Idade Plano Dependente Valor Plano Venc. do Plano\n");
+    printf("Nome                      Sexo Fone        Email                               Idade Plano Dep.      Valor Venc.\n");
     linha();
     rewind(base_clientes);
     fread(&titular_aux, sizeof(Titular), 1, base_clientes);
@@ -184,17 +203,16 @@ void listagem() {
 
     while (feof(base_clientes) == 0) {
         if (titular_aux.sexo != 0) {
-            int idade = anoAtual - atoi(titular_aux.data_nascimento.ano);
+            int idade = anoAtual - titular_aux.data_nascimento.ano;
             int qtdDep;
-            for(qtdDep = 0; qtdDep < sizeof(titular_aux.dependentes) / sizeof(titular_aux.dependentes[0]);) {
-                if(strcmp(titular_aux.dependentes[qtdDep].cpf, "")) {
+            for (qtdDep = 0; qtdDep < sizeof(titular_aux.dependentes) / sizeof(titular_aux.dependentes[0]);) {
+                if (strcmp(titular_aux.dependentes[qtdDep].cpf, "")) {
                     qtdDep++;
                 } else {
                     break;
                 }
             }
-            printf("%-11s %-15s %4d %-11s %-30s %5d %5d %10d %-11.2f %s/%s/%s\n", titular_aux.cpf,
-                   titular_aux.nome, titular_aux.sexo,
+            printf("%-25s %4d %-11s %-35s %5d %5d %-4d %10.2f %d/%d/%d\n", titular_aux.nome, titular_aux.sexo,
                    titular_aux.telefone, titular_aux.email, idade, titular_aux.plano, qtdDep, titular_aux.valorPlano,
                    titular_aux.data_vencimento.dia, titular_aux.data_vencimento.mes, titular_aux.data_vencimento.ano);
         }
